bd43387f049c82617653fa17797001cc67d1633b
Mohammad Harman Rashidi <78035461+TMWicKed@users.noreply.github.com>
2025-05-13 05:47:09 +0000

    Datatypes and ECMA standards

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict" //this tell to teat all JS code as newer version
+
+// alert( 3 + 3 ) //used on browser, we are using nodejs
+
+console.log( 3 
+    +
+    3) //don't write like this as it reduces readability 
+
+console.log("Harman")
+
+
+let name = "Harman"
+let age = 24
+let isLoggedIn = false
+
+
+// number => 2^53
+// bigint
+// spring => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+// objects
+
+console.log(typeof "harman");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
